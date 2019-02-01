f27c5af6d260ad7cc15e65f71ae0c0bd3c55d3ae
Matthew Freeman <matthew.freeman821@gmail.com>
2019-02-01 12:50:17 -0500

    Creates Book class

---
+++ app.js
@@ -0,0 +1,17 @@
+//Book Class (represents a book)
+class Book {
+  constructor(title, author, isbn) {
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
+  }
+}
+//UI Class (handles UI tasks)
+
+//Store Class (handles storage)
+
+//Event to display books
+
+//Event to add a book
+
+//Event to remove a book
